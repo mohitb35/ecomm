441fd2bfe84031f4bf4b92e82481840fc6b4dd78
Mohit Bajaj <mohit_b35@hotmail.com>
2020-02-25 02:29:56 +0530

    add form - render html in GET / route - access form data in POST / route - bodyParser middleware added to parse form data from request

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-	res.send('hi there');
+	res.send(`
+		<div>
+			<form method="POST">
+				<input type="email" name="email" placeholder="email" id="">
+				<input type="password" name="password" placeholder="password" id="">
+				<input type="password" name="confirmPassword" placeholder="confirm password" id="">
+				<button>Sign Up</button>
+			</form>
+		</div>
+	`);
+});
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.send('Account created!!!');
 });
 
 const PORT = process.env.PORT || 3000;
