cdff5d6f1e45548d937a599b362e3d6f67594bbf
Mohit Bajaj <mohit_b35@hotmail.com>
2020-03-09 12:50:17 +0530

    add frontend product list route 1. Add sample data to products.json 2. Add / route in routes/products.js and retrieve product list 3. Refactor index.js  - rename admin product route (remove conflict)

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(cookieSession({
 	keys: ['ansfknwsclwmsn32o32m', 'asijfnweiocj2321w2190']
 }));
 app.use(authRouter);
-app.use(productRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/products.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const productsRepo = require('../repositories/products');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	const products = await productsRepo.getAll();
+	res.send(products);
+})
+
+module.exports = router;
